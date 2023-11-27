a216108991a559ad61d21b5073c75d61405bc53c
Julien Miguel <miguel.julien.pro@gmail.com>
2023-11-27 19:01:01 +0100

    added prettier
    Signed-off-by: Julien Miguel <miguel.julien.pro@gmail.com>

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-"user strict";
+'user strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
 const displayMessage = function (message) {
-  document.querySelector(".message").textContent = message;
+  document.querySelector('.message').textContent = message;
 };
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
   // console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
     // document.querySelector('.message').textContent = '🛑 No number !';
-    displayMessage("🛑 No number !");
+    displayMessage('🛑 No number !');
 
     // When player wins
   } else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = '🎉 Correct Number !';
-    displayMessage("🎉 Correct Number !");
-    document.querySelector(".number").textContent = secretNumber;
+    displayMessage('🎉 Correct Number !');
+    document.querySelector('.number').textContent = secretNumber;
 
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rem";
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
+      document.querySelector('.highscore').textContent = highscore;
     }
 
     // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
       // document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high !' : '📉 Too low !';
-      displayMessage(guess > secretNumber ? "📈 Too high !" : "📉 Too low !");
+      displayMessage(guess > secretNumber ? '📈 Too high !' : '📉 Too low !');
       score--;
-      document.querySelector(".score").textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
       // document.querySelector('.message').textContent = '💥 You lost the game !';
-      displayMessage("💥 You lost the game !");
-      document.querySelector(".score").textContent = 0;
+      displayMessage('💥 You lost the game !');
+      document.querySelector('.score').textContent = 0;
     }
   }
   // When guess is too high
@@ -70,7 +70,7 @@ document.querySelector(".check").addEventListener("click", function () {
   */
 });
 
-document.querySelector(".again").addEventListener("click", function () {
+document.querySelector('.again').addEventListener('click', function () {
   // Inside the event listener function :
   // Reset the score and secretNumber variables to their initial values
   score = 20;
@@ -79,12 +79,12 @@ document.querySelector(".again").addEventListener("click", function () {
   // Reset the text content of the elements with classes :
   // 'message', 'number', 'score' and the guess input filed
   // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage("Start guessing...");
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".guess").textContent = "";
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').textContent = '';
 
   // Reset the background color and number width of the relevent elements
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
